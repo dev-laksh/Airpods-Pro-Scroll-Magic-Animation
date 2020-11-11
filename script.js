ff31cf774b859ec451630ef9f99c7c6191f15fe5
dev-laksh <70800648+dev-laksh@users.noreply.github.com>
2020-11-11 14:27:03 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+// END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+// SCROLL MAGIC
+const controller = new ScrollMagic.Controller();
+// SCENES
+let scene = new ScrollMagic.Scene({
+    duration: 2000,
+    triggerElement: intro,
+    triggerHook: 0,
+})
+    .addIndicators()
+    .setPin(intro)
+    .addTo(controller);
+
+// TEXT ANIMATION
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+    duration: 3000,
+    triggerElement: intro,
+    triggerHook: 0
+})
+    .setTween(textAnim)
+    .addTo(controller);
+
+// VIDEO ANIMATION
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", e => {
+    scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+    delay += (scrollpos - delay) * accelamount;
+    console.log(scrollpos, delay);
+
+    video.currentTime = scrollpos;
+}, 1.3)
